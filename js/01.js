d1917dcf8626bceafb237ed602ecb7faafa12b6f
Jair <jairaceves56@gmail.com>
2022-03-01 12:15:54 -0600

    Variable let

--- js/01.js
+++ js/01.js
@@ -5,3 +5,18 @@
 // -CONST
 
 
+let cliente = "Carlos"
+let precioDescuento = 220 // camelCase
+
+// Re Asignar
+cliente = 20
+cliente = true
+
+// Pueden iniciar sin ningun valor
+let precio
+precio = 1000
+
+console.log(precio);
+
+console.log(cliente);
+console.log(precioDescuento);
