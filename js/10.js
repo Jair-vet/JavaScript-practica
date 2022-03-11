42ad31554991e011f49317a2cd316d5d0ce1f2a7
Jair <jairaceves56@gmail.com>
2022-03-11 14:50:34 -0600

    Destructuring de Arrays

---
+++ js/10.js
@@ -0,0 +1,9 @@
+// Destructuring de Arrays
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS']
+
+// Se extraey deacuerdo a la posición
+const [ , , , , node] = tecnologias // Puedes usar comas
+
+
+console.log(node);
+
