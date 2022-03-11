97e63d88a38104e7acc98aa17b1d0f506dd059d4
Jair <jairaceves56@gmail.com>
2022-03-11 15:43:37 -0600

    Function Declaration

---
+++ js/11.js
@@ -0,0 +1,20 @@
+// Iteradores en JS
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React', 'NodeJS']
+
+// forEach  // Se ejecuta las veces de elementos que hay en el Arreglo
+// Te permite listar un arreglo
+const arrayForEach = tecnologias.forEach( function(tech){
+    return tech
+} )
+
+// Map   
+// Te permite crearun nuevo arreglo
+const arrayMap = tecnologias.map( function(tech){
+    return tech
+} )
+
+
+console.log(arrayForEach);
+console.log(arrayMap);
+
+
---
+++ js/12.js
@@ -0,0 +1,8 @@
+// Funciones -Function Declaration
+
+function sumar() {
+    console.log(2 + 2);
+}
+
+
+sumar()
