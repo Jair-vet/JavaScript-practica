2b804e2b16e72428d1e607c96228a6a763e3ac8a
Jair <jairaceves56@gmail.com>
2022-03-12 17:34:13 -0600

    Selectores del DOM

---
+++ js/22.js
@@ -0,0 +1,12 @@
+// Selectores del DOM
+
+const heading = document.querySelector('h2')
+// Nos retorna unicamente un elemento 
+
+
+const enlaces = document.querySelectorAll('.navegacion a')
+// Nos retorna todas las coincidencias del selector
+
+console.log(heading);
+console.log(enlaces);
+
