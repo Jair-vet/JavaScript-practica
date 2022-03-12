ce96519bcfbf95079d7df2024daf25930070aedc
Jair <jairaceves56@gmail.com>
2022-03-12 16:56:41 -0600

    Condicional If

---
+++ js/17.js
@@ -0,0 +1,20 @@
+// Condicionales
+
+const disponible = 4000
+const retirar = 200
+
+if ( disponible > retirar ){
+    // Si cumple la condición y se ejecuta el código
+    console.log('Si puedes Retirar');
+}else{
+    // No se cumple la condicón, ejecutar este código
+    console.log('Lo siento No puedes Retirar');
+}
+
+/*
+    * >   -  Mayor Que
+    * <   -  Menor Que
+    * >=  -  Mayor o Igual
+    * <=  -  Menor o Igual
+    * === -  Igual que
+*/
