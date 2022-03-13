ebbb67c19bef27866a5ba6ac05624a760c96286f
Jair <jairaceves56@gmail.com>
2022-03-12 18:01:13 -0600

    Async/Await

---
+++ js/30.js
@@ -0,0 +1,24 @@
+// Fetch API - Async / Await
+
+const url = "https://jsonplaceholder.typicode.com/comments"
+
+
+// const consultarApi = () => {
+// 
+//     fetch(url).then( (resp) => {
+//         return resp.json()
+//     })
+//     .then( (result) => {
+//         result.forEach( comentario => {
+//             console.log(comentario);
+//         })
+//     })
+// }
+
+const consultarApi = async () => {
+    const respuesta = await fetch(url)
+    const resultado = await respuesta.json()
+
+    console.log(resultado);
+}
+consultarApi()
